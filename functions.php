f56f74c686112c57a91df8e5ce9f82b0a659ea78
Nisha Agrawal <agrawalnisha.096@gmail.com>
2026-07-08 18:15:32 +0000

    Fix functions.php syntax corrupted by terminal paste

--- functions.php
+++ functions.php
@@ -6,10 +6,10 @@
 add_action( 'wp_enqueue_scripts', 'bloghash_child_enqueue_styles' );
 
 function bloghash_child_enqueue_styles() {
-style.css wp_enqueue_style(
-style.css style.css 'bloghash-child-styles',
-style.css style.css get_stylesheet_directory_uri() . '/style.css',
-style.css style.css array( 'bloghash-styles' ),
-style.css style.css wp_get_theme()->get( 'Version' )
-style.css );
+	wp_enqueue_style(
+		'bloghash-child-styles',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( 'bloghash-styles' ),
+		wp_get_theme()->get( 'Version' )
+	);
 }
